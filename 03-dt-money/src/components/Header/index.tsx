a8cfdb6ff9641a9d39caa97ee9f96da4eafc0a6b
Victor Gianelli <victorgnll@gmail.com>
2023-03-27 22:33:26 -0300

    Projeto 03 - Conexão com a API

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -1,8 +1,8 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 
-import logoImg from "../../assets/logo.svg";
-import * as Dialog from "@radix-ui/react-dialog";
-import { NewTransactionModal } from "../NewTransactionModal";
+import logoImg from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
@@ -11,13 +11,13 @@ export function Header() {
         <img src={logoImg} alt="" />
 
         <Dialog.Root>
-        <Dialog.Trigger asChild>
-          <NewTransactionButton>Nova transação</NewTransactionButton>
-        </Dialog.Trigger>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
 
-        <NewTransactionModal />
-      </Dialog.Root>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
-  );
+  )
 }
