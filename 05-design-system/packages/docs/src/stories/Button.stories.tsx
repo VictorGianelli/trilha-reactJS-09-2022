4543711e0828c35597a2e76a1acdc335b3c0de23
Victor Gianelli <victorgnll@gmail.com>
2023-04-13 21:32:40 -0300

    Projeto 05 - Design System em React

--- 05-design-system/packages/docs/src/stories/Button.stories.tsx
+++ 05-design-system/packages/docs/src/stories/Button.stories.tsx
@@ -1,9 +1,8 @@
-
 import type { Meta, StoryObj } from '@storybook/react'
 import { Button, ButtonProps } from '@ignite-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
 
   args: {
