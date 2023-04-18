61d46c88d4af86a6d06b937d2563aa7b2c1e7c38
Victor Gianelli <victorgnll@gmail.com>
2023-04-18 19:18:32 -0300

    Controles nos componentes

--- 05-design-system/packages/docs/src/stories/Avatar.stories.tsx
+++ 05-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/victorgianelli.png',
     alt: 'Victor Gianelli',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
